e4a3081293870ab7cbea757dd468ddb3f9ce09e8
Jacob Uphoff <jacobsuphoff@me.com>
2019-03-05 14:13:37 +0000

    Adding a modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,10 +3,6 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote = Quote.new
-  end
-
   def create
     @quote = Quote.create(quote_params)
   if @quote.invalid?
@@ -15,6 +11,9 @@ class QuotesController < ApplicationController
     redirect_to root_path
   end
 
+
+def about
+end
 
   private
 
